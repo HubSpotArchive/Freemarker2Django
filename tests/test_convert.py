3d08e71a0520081b9ebdf49f60c494560af2fcc4
Owen Raccuglia <ojracc@gmail.com>
2011-04-04 00:15:29 -0400

    Fixed some camelCase.

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -21,7 +21,7 @@ FREEMARKER_BASIC_COMMENT_NAME = 'freemarker_basic_comment.ftl'
 DJANGO_BASIC_COMMENT_NAME = 'django_basic_comment.html'
 
 class TestConvert(BaseTest):
-    def testBasicConvert(self):
+    def test_basic_convert(self):
         """ Tests conversion with no tokens are macros. """
         template = convert.get_template(resource(FREEMARKER_BASIC_HTML_NAME))
         output = convert.freemarker_to_django(template)
@@ -29,18 +29,11 @@ class TestConvert(BaseTest):
         expected = read_file(DJANGO_BASIC_HTML_NAME)
         self.assertEqualsStr(expected, output.replace('\r', ''))
 
-    def testConvertBasicVariable(self):
+    def test_convert_basic_variable(self):
         """ Tests conversion of variable to something that Django can understand. """
         template = convert.get_template(resource(FREEMARKER_BASIC_VARIABLE_NAME))
         output = convert.freemarker_to_django(template)
         expected = read_file(DJANGO_BASIC_VARIABLE_NAME)
-        self.assertEqualsStr(expected, output)
-
-    def testConvertBasicVariable(self):
-        """ Tests conversion of variable to something that Django can understand. """
-        template = convert.get_template(resource(FREEMARKER_BASIC_COMMENT_NAME))
-        output = convert.freemarker_to_django(template)
-        expected = read_file(DJANGO_BASIC_COMMENT_NAME)
         self.assertEqualsStr(expected, output)
         
 if __name__ == '__main__':
